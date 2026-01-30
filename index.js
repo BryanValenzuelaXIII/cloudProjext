dfc46f64a4213fcf05dcfba94def9ae8cd52869c
Bryan Valenzuela <bryan99foo@gmail.com>
2026-01-30 14:18:44 -0500

    Initial commit

--- index.js
+++ index.js
@@ -7,20 +7,23 @@ const matchRoute = require("./routes/match");
 app.use(express.json());
 // CRUD Match
 app.use("/api/matches", matchRoute);
-
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m9x5xch.mongodb.net/matches-API?retryWrites=true`
-  )
-  .then(() => {
-    console.log("Database connection established ");
-    app.listen(3000, () => {
+ app.listen(3000, () => {
       console.log("Listening at port 3000");
     });
-  })
-  .catch((err) => {
-    console.log("Connection Failed: ", err);
-  });
+
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m9x5xch.mongodb.net/matches-API?retryWrites=true`
+//   )
+//   .then(() => {
+//     console.log("Database connection established ");
+//     app.listen(3000, () => {
+//       console.log("Listening at port 3000");
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("Connection Failed: ", err);
+//   });
 
 app.get("/", (req, res) => {
   res.send("Matches API at your service");
